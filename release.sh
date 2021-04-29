bf83e59c59d3ead98c431bbcfc665f04e4f5ea34
Huchell <sean.p.murray97@gmail.com>
2021-04-29 17:20:48 +0100

    Update release workflow.

--- release.sh
+++ release.sh
@@ -1,5 +1,9 @@
 git branch -b $RELEASE_BRANCH main
+
+cd com.huchell.test-ci-package
 npm version $VERSION
+cd ..
+
 git add package.json
 git commit -m "Bump version"
 git push --set-upstream origin $RELEASE_BRANCH
