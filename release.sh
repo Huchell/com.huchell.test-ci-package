fe388d18bc108593e2750e0d36414ce14bc67275
Huchell <sean.p.murray97@gmail.com>
2021-04-29 17:35:19 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@ git checkout -b $RELEASE_BRANCH main
 
 cd com.huchell.test-ci-package
 npm version $VERSION
+git add package.json
 cd ..
 
-git add package.json
 git commit -m "Bump version"
 git push --set-upstream origin $RELEASE_BRANCH
